2245ca5e99ee5791b0015073c7781ad2e2f9f986
Jivko Uzunov <jivkou@yahoo.com>
2021-09-04 01:48:14 +0300

    Create Same numbers.js

---
+++ Syntax, Functions and Statements/Same numbers.js
@@ -0,0 +1,20 @@
+function solution(param) {
+    let sameDigitsChecker = true;
+    let sum = 0;
+    // var num;
+    if(typeof(param) != `string`){
+        num = param.toString();
+    }
+    else {
+        num = param;
+    }
+    for(i = 0; i < num.length; i++){
+        if(num[0] != num[i]){
+            sameDigitsChecker = false;
+        }
+        sum += parseInt(num[i]);
+    }
+    console.log(sameDigitsChecker);
+    console.log(sum);
+}
+solution(2222222)
