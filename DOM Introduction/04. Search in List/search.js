f56e9fa39842b2383f790e32ecbd1dc676fe323f
jivko25 <jivkou@yahoo.com>
2021-10-12 23:56:29 +0300

    Update search.js

--- DOM Introduction/04. Search in List/search.js
+++ DOM Introduction/04. Search in List/search.js
@@ -1,3 +1,14 @@
 function search() {
-   // TODO
+   const items = document.getElementsByTagName("li");
+   const searchedItem = document.getElementById("searchText");
+   const result = document.getElementById("result");
+   let matches = 0;
+   for(let i = 0; i < items.length; i++){
+      if(items[i].innerHTML.includes(searchedItem.value)){
+         items[i].style.fontWeight = "bold";
+         items[i].style.textDecoration = "underline";
+         matches += 1;
+      }
+   }
+   result.textContent = `${matches} matches found`
 }
