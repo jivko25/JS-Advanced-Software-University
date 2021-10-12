7723d50e0c18c2bd398b0b169eeca18911bf7898
jivko25 <jivkou@yahoo.com>
2021-10-12 18:38:14 +0300

    Update accordion.js

--- DOM Introduction/03. Accordion/accordion.js
+++ DOM Introduction/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    const button = document.querySelector(".button");
+    const textContainer = document.getElementById("extra");
+    if(button.textContent == "Less"){
+        button.textContent = "More";
+        textContainer.style.display = "none";
+    }
+    else{
+        button.textContent = "Less";
+        textContainer.style.display = "block";
+    }
 }
