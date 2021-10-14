ea426e1d0b004a57d7508f008d6e50dcec15ccd3
jivko25 <jivkou@yahoo.com>
2021-10-14 17:00:18 +0300

    Update solution.js

--- DOM Introduction/06. Format the Text/solution.js
+++ DOM Introduction/06. Format the Text/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-  //TODO
+  const input = document.getElementById("input");
+  const output = document.getElementById("output");
+
+  const sentences = input.value.split(".");
+  let completedSentences = 0;
+  for(let i = 0; i < sentences.length; i++){
+    if(sentences[i].length < 2){
+      continue;
+    }
+    else{
+      if(completedSentences % 3 == 0){
+      let p = document.createElement("p");
+      p.textContent = sentences[i]
+      output.appendChild(p)
+      }
+      else{
+      let p = document.querySelectorAll("p");
+      p[p.length - 1].textContent += sentences[i];
+      }
+      completedSentences += 1;
+    }
+  }
 }
