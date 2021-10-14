935b9df39ebee49e31ef936c5fe9caea8c88ceb6
jivko25 <jivkou@yahoo.com>
2021-10-14 16:33:51 +0300

    Update solution.js

--- DOM Introduction/05. Table - Search Engine/solution.js
+++ DOM Introduction/05. Table - Search Engine/solution.js
@@ -2,7 +2,16 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
-
+     const items = Array.from(document.querySelectorAll("tbody tr"));
+     const input = document.getElementById("searchField").value.toLowerCase();
+     items.forEach(element => {
+        let text = element.textContent.toLowerCase();
+        if(text.includes(input)){
+           element.classList.add("select");
+        } else{
+           element.classList.remove("select");
+        }
+     });
+     document.getElementById("searchField").value = "";
    }
 }
