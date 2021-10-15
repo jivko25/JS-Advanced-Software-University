aafd9981af6f55983da52c5c649c974d2585074c
jivko25 <jivkou@yahoo.com>
2021-10-16 01:00:56 +0300

    Update app.js

--- DOM Events/03. Locked-Profile/app.js
+++ DOM Events/03. Locked-Profile/app.js
@@ -1,3 +1,24 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const profiles = document.querySelectorAll(".profile");
+    // profiles.forEach(el => {
+    //     el.querySelector("button").addEventListener('click', onClick);
+    // })
+
+    for(let i = 0; i < profiles.length; i++){
+        // const isLocked = profiles[i].parentElement.querySelector('input[value="lock"]').checked;
+        profiles[i].querySelector("button").addEventListener('click', onClick);
+    }
+
+    function onClick(e) {
+        const isLocked = e.target.parentElement.querySelector('input[value="lock"]').checked;
+        if(e.target.textContent == "Show more" && !isLocked)
+        {
+            e.target.textContent = "Hide it";
+            e.target.parentElement.querySelector("div").style.display = 'block';
+        }
+        else if(!isLocked){
+            e.target.textContent = "Show more";
+            e.target.parentElement.querySelector("div").style.display = 'none'; 
+        }
+    }
 }
