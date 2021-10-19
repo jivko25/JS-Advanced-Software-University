d08f629f0fd5509c67147643d44b761184db4d96
jivko25 <jivkou@yahoo.com>
2021-10-20 01:28:47 +0300

    add

--- Unit Testing and Error Handling/module.js
+++ Unit Testing and Error Handling/module.js
@@ -9,8 +9,20 @@ function isOddOrEven(string) {
     return "odd";
 }
 
+function lookupChar(string, index) {
+    if (typeof(string) !== 'string' || !Number.isInteger(index)) {
+        return undefined;
+    }
+    if (string.length <= index || index < 0) {
+        return "Incorrect index";
+    }
+
+    return string.charAt(index);
+}
+
 
 
 module.exports = {
-    isOddOrEven
+    isOddOrEven, 
+    lookupChar
 }
--- Unit Testing and Error Handling/test.js
+++ Unit Testing and Error Handling/test.js
@@ -1,19 +1,42 @@
 const expect = require('chai').expect;
-const { isOddOrEven } = require('./module');
+const { isOddOrEven, lookupChar } = require('./module');
 
-describe("isOddOrEven Tests", () => {
-    it("Test with invalid input", () => {
-        expect(isOddOrEven(3)).to.be.undefined;
-        expect(isOddOrEven([])).to.be.undefined;
-        expect(isOddOrEven({})).to.be.undefined;
+//Tests for isOddOrEven
+// describe("isOddOrEven Tests", () => {
+//     it("Test with invalid input", () => {
+//         expect(isOddOrEven(3)).to.be.undefined;
+//         expect(isOddOrEven([])).to.be.undefined;
+//         expect(isOddOrEven({})).to.be.undefined;
+//     });
+
+//     it("Test with even input", () => {
+//         expect(isOddOrEven("even")).to.equal("even");
+//         expect(isOddOrEven("")).to.equal("even");
+//     })
+
+//     it("Test with odd input", () => {
+//         expect(isOddOrEven("odd")).to.equal("odd");
+//     })
+// })
+
+describe("lookupChar Tests", () => {
+    it("Tests with invalid input", () => {
+        expect(lookupChar(1, 3)).to.be.undefined;
+        expect(lookupChar([], 3)).to.be.undefined;
+        expect(lookupChar({}, 3)).to.be.undefined;
+        expect(lookupChar('1', '1')).to.be.undefined;
+        expect(lookupChar('1', 1.1)).to.be.undefined;
     });
 
-    it("Test with even input", () => {
-        expect(isOddOrEven("even")).to.equal("even");
-        expect(isOddOrEven("")).to.equal("even");
+    it("Tests with incorrect index", () => {
+        expect(lookupChar("test", 10)).to.equal("Incorrect index");
+        expect(lookupChar("test", -1)).to.equal("Incorrect index");
+        expect(lookupChar("test", 100)).to.equal("Incorrect index");
     })
 
-    it("Test with odd input", () => {
-        expect(isOddOrEven("odd")).to.equal("odd");
+    it("Test with correct input", () => {
+        expect(lookupChar("test", 1)).to.equal('e');
+        expect(lookupChar("tests", 4)).to.equal('s');
+        expect(lookupChar("new hope", 3)).to.equal(' ');
     })
 })
