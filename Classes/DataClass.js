1a4567f95ca0f0e14986d05ae2e8afcd5b642cfe
jivko25 <jivkou@yahoo.com>
2021-10-20 19:53:26 +0300

    add

---
+++ Classes/DataClass.js
@@ -0,0 +1,21 @@
+class Request{
+    constructor(method, uri, version, message, response, fulfilled){
+        this.method = method;
+        this.uri = uri;
+        this.version = version;
+        this.message = message;
+        this.response = undefined;
+        this.fulfilled = false;
+    }
+}
+
+let myData = new Request('GET', 'http://google.com', 'HTTP/1.1', '')
+console.log(myData);
+
+
+// •	method (String)
+// •	uri (String)
+// •	version (String)
+// •	message (String)
+// •	response (String)
+// •	fulfilled (Boolean)
