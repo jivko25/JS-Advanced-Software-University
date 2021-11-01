c6126ece77df8230e197f51131213bab1fd1378d
jivko25 <jivkou@yahoo.com>
2021-11-01 13:58:12 +0200

    add Bus-Schedule task + fix Bus-Stop task

--- JS-Application/Asynchonomus programming/01.Bus-Stop/app.js
+++ JS-Application/Asynchonomus programming/01.Bus-Stop/app.js
@@ -3,7 +3,12 @@ function getInfo() {
     const stopName = document.getElementById('stopName');
     const busesElement = document.getElementById('buses');
     fetch(`http://localhost:3030/jsonstore/bus/businfo/${input}`)
-    .then(res => res.json())
+    .then(res => {
+        if(!res.ok || input.trim() == ''){
+            throw new Error("Error");
+        };
+        return res.json();
+    })
     .then(data => {
         busesElement.replaceChildren();
         stopName.textContent = data.name;
@@ -12,6 +17,10 @@ function getInfo() {
             li.textContent = `Bus ${bus} arrives in ${data.buses[bus]} minutes`;
             busesElement.appendChild(li);
         }
-        });
+        })
+    .catch(error => {
+        busesElement.replaceChildren();
+        stopName.textContent = 'Error';
+    });
     input.value = '';
 }
--- JS-Application/Asynchonomus programming/02.Bus-Schedule/app.js
+++ JS-Application/Asynchonomus programming/02.Bus-Schedule/app.js
@@ -1,11 +1,32 @@
 function solve() {
+    const info = document.getElementById('info').querySelector('.info');
+    const departButton = document.getElementById('depart');
+    const arriveButton = document.getElementById('arrive');
+
+    let url = 'http://localhost:3030/jsonstore/bus/schedule/depot';
 
     function depart() {
-        console.log('Depart TODO...');
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            info.textContent = `Next stop ${data.name}`;
+        })
+        
+
+        departButton.disabled = true;
+        arriveButton.disabled = false;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            url = `http://localhost:3030/jsonstore/bus/schedule/${data.next}`;
+            info.textContent = `Arriving at ${data.name}`;
+        })
+
+        departButton.disabled = false;
+        arriveButton.disabled = true;
     }
 
     return {
